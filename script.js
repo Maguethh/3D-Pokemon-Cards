c9db750ac8bedd48427f1d523fda41491c7c21fe
Magueth <magueth17@hotmail.com>
2023-12-09 20:32:08 +0100

    Finalisation v1
    - Ajout des finalisations des cards
    
    - Ajout de nuages ainsi que d'une animation
    
    (crédit à udemy pour les nuages)

--- script.js
+++ script.js
@@ -10,9 +10,18 @@ card.forEach((el) => {
     let midCardWidth = elRect.width / 2;
     let midCardHeight = elRect.height / 2;
 
-    let angleY = (x - midCardWidth) / 8;
+    let angleY = -(x - midCardWidth) / 8;
     let angleX = (y - midCardHeight) / 8;
 
+    let glowX = (x / elRect.width) * 100;
+    let glowY = (y / elRect.height) * 100;
+
     el.children[0].style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg) scale(1.1)`;
+    el.children[1].style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg) scale(1.1)`;
+    el.children[1].style.background = `radial-gradient(circle at ${glowX}% ${glowY}%, rgb(230, 252, 245), transparent)`;
+  });
+  el.addEventListener("mouseleave", () => {
+    el.children[0].style.transform = "rotateX(0) rotateY(0)";
+    el.children[1].style.transform = "rotateX(0) rotateY(0)";
   });
 });
